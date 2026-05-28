2acfe475ab1c8f9d8dcc52a385dcf404fc3765d7
DMen6L <240103073@sdu.edu.kz>
2026-05-28 17:16:06 +0500

    feat: color shenanigans
    templates for future dynamic color toggling

---
+++ .config/assets/palettes/gruvbox.lua
@@ -0,0 +1,24 @@
+local dark_mode = {
+	name = "gruvbox_dark_mode",
+	bg0 = {
+		hex = "282828",
+	},
+	bg1 = {
+		hex = "3c3836",
+	},
+	bg2 = {
+		hex = "504945",
+	},
+	bg3 = {
+		hex = "665c54",
+	},
+	bg4 = {
+		hex = "7c6f64",
+	},
+}
+
+return {
+	dark_mode = function()
+		return dark_mode
+	end,
+}

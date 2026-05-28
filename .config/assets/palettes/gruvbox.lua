4ee61a99fbd6f54999100349260299c75bda640f
DMen6L <240103073@sdu.edu.kz>
2026-05-28 21:32:23 +0500

    fix: fixing gruvbox hex colors

--- .config/assets/palettes/gruvbox.lua
+++ .config/assets/palettes/gruvbox.lua
@@ -1,19 +1,22 @@
 local dark_mode = {
 	name = "gruvbox_dark_mode",
 	bg0 = {
-		hex = "282828",
+		hex = "#282828",
+	},
+	bg0_h = {
+		hex = "#1d2021",
 	},
 	bg1 = {
-		hex = "3c3836",
+		hex = "#3c3836",
 	},
 	bg2 = {
-		hex = "504945",
+		hex = "#504945",
 	},
 	bg3 = {
-		hex = "665c54",
+		hex = "#665c54",
 	},
 	bg4 = {
-		hex = "7c6f64",
+		hex = "#7c6f64",
 	},
 }
 
